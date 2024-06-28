1ca0997eea56156232e46c43bfe2ef4f83fa7895
admin <admin@MacBook-Air-admin.local>
2024-06-28 22:12:44 +0300

    add file

---
+++ functions.py
@@ -0,0 +1,9 @@
+print("hello world")
+
+# Визначення функції з двома параметрами
+def add_numbers(num1, num2):
+    sum = num1 + num2
+    print("Sum: ",sum)
+ 
+# Виклик функції з вказанням двох аргументів
+add_numbers(5, 4)
